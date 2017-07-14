f16513857a8d3c2971fa9baebc0fbf156c953c41
有话丶好好说 <591005835@qq.com>
2017-07-15 00:09:27 +0800

    listend

--- src/common/js/song.js
+++ src/common/js/song.js
@@ -16,7 +16,7 @@ export function createSong(musicData) {
     id: musicData.songid,
     mid: musicData.songmid,
     singer: filterSinger(musicData.singer),
-    name: musicDate.songname,
+    name: musicData.songname,
     album: musicData.albumname,
     duration: musicData.interval,
     image: `https://y.gtimg.cn/music/photo_new/T002R300x300M000${musicData.albummid}.jpg?max_age=2592000`,
@@ -29,7 +29,7 @@ function filterSinger(singer) {
   if (!singer) {
     return ''
   }
-  singer.forEach(() => {
+  singer.forEach((s) => {
     ret.push(s.name)
   })
   return ret.join('/')
--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,6 +1,6 @@
 <template>
   <transition name="slide">
-    <div class="singer-detail"></div>
+    <music-list :title="title" :bg-image="bgImage" :songs="songs"></music-list>
   </transition>
 </template>
 
@@ -9,6 +9,7 @@
   import {getSingerDetail} from 'api/singer'
   import {ERR_OK} from 'api/config'
   import {createSong} from 'common/js/song'
+  import MusicList from 'components/music-list/music-list'
 
   export default {
     data() {
@@ -16,7 +17,16 @@
         songs: []
       }
     },
+    components: {
+      MusicList
+    },
     computed: {
+      title() {
+        return this.singer.name
+      },
+      bgImage() {
+        return this.singer.avatar
+      },
       ...mapGetters([
         'singer'
       ])
@@ -51,15 +61,6 @@
 
 <style scoped lang="stylus" type="text/stylus" rel="stylesheet/stylus">
   @import "~common/stylus/variable"
-
-  .singer-detail
-    position: fixed
-    top: 0
-    left: 0
-    right: 0
-    bottom: 0
-    z-index: 100
-    background: $color-background
 
   .slide-enter-active, .slide-leave-active
     transition: all 0.3s
